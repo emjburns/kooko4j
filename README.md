00bcd76db5b3d9d24f079c1732e4943574145cfb
emjburns <emjeburns@gmail.com>
2016-07-25 11:05:41 -0700

    adding logging configuration

---
+++ README.md
@@ -0,0 +1,7 @@
+To run, use the run script
+
+```
+./run.sh
+```
+
+To run without logging, change the run script to the no logging option.
--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-java -jar target/kooko4j-1.0-SNAPSHOT.jar
+PRJ="$(cd `dirname $0`; pwd)"
+
+java -Dlog4j.configuration=file:${PRJ}/src/main/resources/log4j.properties -jar target/kooko4j-1.0-SNAPSHOT.jar
+
+# use this for no logging
+#java -jar target/kooko4j-1.0-SNAPSHOT.jar
